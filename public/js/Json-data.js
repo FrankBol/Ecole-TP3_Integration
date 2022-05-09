59e0ffaffc8dc02d01c8b9cfa322c9cba81dbf7f
Francis Bolduc <f.bolduc@outlook.com>
2022-05-09 19:41:19 -0400

    ww

--- public/js/Json-data.js
+++ public/js/Json-data.js
@@ -1,47 +1,3 @@
-// let questionsJson = `[
-//     {
-//     "numero":"1",
-//     "question":"À quoi sert un aria-label?",
-//     "réponses":[
-//     "Ajouter du contenu textuel sur une balise pour aider les lecteurs d'écran",
-//     "À rien", 
-//     "Je ne sais pas"
-//     ], 
-//     "réponse":2
-// },
-// {
-//     "numero":"2",
-//     "question":"HTML vient de :",
-//     "réponses":[
-//         "Hyper Typo Meta Lol",
-//         "Hypertext markup language", 
-//         "Je ne sais pas"
-//     ], 
-//     "réponse":2
-// },
-// {
-//     "numero":"3",
-//     "question":"HTML  :",
-//     "réponses":[
-//         "Hyper Typo Meta Lol",
-//         "Hypertext markup language", 
-//         "Je ne sais pas"
-//     ], 
-//     "réponse":2
-// },
-// {
-//     "numero":"4",
-//     "question":"HTML  :",
-//     "réponses":[
-//         "Hyper Typo Meta Lol",
-//         "Hypertext markup language", 
-//         "Je ne sais pas"
-//     ], 
-//     "réponse":2
-// }
-// ]`
-
-
 let questionsJson = `[
     {
     "numero":"1",
@@ -62,5 +18,49 @@ let questionsJson = `[
         "Je ne sais pas"
     ], 
     "réponse":2
+},
+{
+    "numero":"3",
+    "question":"HTML  :",
+    "réponses":[
+        "Hyper Typo Meta Lol",
+        "Hypertext markup language", 
+        "Je ne sais pas"
+    ], 
+    "réponse":2
+},
+{
+    "numero":"4",
+    "question":"HTML  :",
+    "réponses":[
+        "Hyper Typo Meta Lol",
+        "Hypertext markup language", 
+        "Je ne sais pas"
+    ], 
+    "réponse":2
 }
 ]`
+
+
+// let questionsJson = `[
+//     {
+//     "numero":"1",
+//     "question":"À quoi sert un aria-label?",
+//     "réponses":[
+//     "Ajouter du contenu textuel sur une balise pour aider les lecteurs d'écran",
+//     "À rien", 
+//     "Je ne sais pas"
+//     ], 
+//     "réponse":2
+// },
+// {
+//     "numero":"2",
+//     "question":"HTML vient de :",
+//     "réponses":[
+//         "Hyper Typo Meta Lol",
+//         "Hypertext markup language", 
+//         "Je ne sais pas"
+//     ], 
+//     "réponse":2
+// }
+// ]`
